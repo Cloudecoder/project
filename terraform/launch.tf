00f15f779896844dc4f2c9aac152ec04972998b8
cloudecoder <sandeepnet45@gmail.com>
2021-07-11 20:54:32 +0530

    terra

---
+++ terraform/launch.tf
@@ -0,0 +1,44 @@
+resource "aws_spot_instance_request" "launch" {
+  count          = 2
+  ami            = "ami-0dc2d3e4c0f9ebd18"
+  instance_type  = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+
+}
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+
+  }
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "http"
+    cidr_blocks      = ["0.0.0.0/0"]
+
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
+provider "aws" {
+  region = "us-east-1"
+}
