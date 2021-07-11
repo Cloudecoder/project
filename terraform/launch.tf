4820cbd2b72e5c1b47e85fb2b21824562d9f3b53
cloudecoder <sandeepnet45@gmail.com>
2021-07-12 01:05:51 +0530

    new

--- terraform/launch.tf
+++ terraform/launch.tf
@@ -1,14 +1,16 @@
-//resource "aws_instance" "sample" {
-//  ami                    = "ami-0dc2d3e4c0f9ebd18"
-//  instance_type          = "t2.micro"
-//
+
+//resource "aws_spot_instance_request" "launch" {
+//  count = 2
+//  ami = "ami-0dc2d3e4c0f9ebd18"
+//  instance_type = "t2.micro"
+//  spot_price = "0.0116"
+//  vpc_security_group_ids = ["sg-078ae966242083129"]
 //}
-resource "aws_spot_instance_request" "launch" {
-  count = 2
-  ami = "ami-0dc2d3e4c0f9ebd18"
-  instance_type = "t2.micro"
-  spot_price = "0.0116"
-  vpc_security_group_ids = ["sg-078ae966242083129"]
+resource "aws_instance" "sample" {
+  count                  = 2
+  ami                    = "ami-0dc2d3e4c0f9ebd18"
+  instance_type          = "t2.micro"
+  key_name               = "key-09a6f072534d94385"
 }
 
 terraform {
