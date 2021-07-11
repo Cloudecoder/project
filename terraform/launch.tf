9b64427091427986e8eabd848f71b6c19dfc1af5
cloudecoder <sandeepnet45@gmail.com>
2021-07-12 01:16:08 +0530

    new

--- terraform/launch.tf
+++ terraform/launch.tf
@@ -10,7 +10,7 @@ resource "aws_instance" "sample" {
   count                  = 2
   ami                    = "ami-0dc2d3e4c0f9ebd18"
   instance_type          = "t2.micro"
-  key_name               = "key-09a6f072534d94385"
+  key_name               = "mykey"
 }
 
 terraform {
