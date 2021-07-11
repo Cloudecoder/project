a93d3d169ab3b74bfac5579b8fc2088f95474b65
cloudecoder <sandeepnet45@gmail.com>
2021-07-11 21:13:24 +0530

    commit

--- terraform/launch.tf
+++ terraform/launch.tf
@@ -2,7 +2,7 @@ resource "aws_spot_instance_request" "launch" {
   count          = 2
   ami            = "ami-0dc2d3e4c0f9ebd18"
   instance_type  = "t2.micro"
-  vpc_security_group_ids = "sg-078ae966242083129"
+  vpc_security_group_ids = ["sg-078ae966242083129"]
 
 }
 provider "aws" {
