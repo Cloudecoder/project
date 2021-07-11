e8acde752f0258b75c9b0bbabee8fdcd3afa7c37
cloudecoder <sandeepnet45@gmail.com>
2021-07-11 21:38:26 +0530

    commit

--- terraform/launch.tf
+++ terraform/launch.tf
@@ -42,6 +42,11 @@ resource "aws_spot_instance_request" "launch" {
 //  }
 //}
 
-provider "aws" {
-  region = "us-east-1"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
 }
