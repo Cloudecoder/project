5b95fdaa0c3bb0559b58d20bdf34f5fd4b4e9f5d
cloudecoder <sandeepnet45@gmail.com>
2021-07-19 21:47:15 +0530

    nw

--- terraform/launch.tf
+++ terraform/launch.tf
@@ -19,7 +19,7 @@ resource "null_resource" "ansible-apply" {
     connection {
       host            = "ec2-100-26-173-250.compute-1.amazonaws.com"
       user            = "ec2-user"
-      ssh_private_key     = file("/roles/mykey.pem")
+      private_key     = file("/roles/mykey.pem")
     }
 
     inline = [
